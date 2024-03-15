9045b1e5ea2a7f36480042f8adda57c246f4ccce
João eduardo <160231914+junaofnetwork@users.noreply.github.com>
2024-03-15 07:23:50 -0300

    Create provaati01.py

---
+++ provaati01.py
@@ -0,0 +1,44 @@
+
+
+# primeira pergunta
+nome_da_pessoa = str(input("digite o nome do individuo: "))
+idade_da_pessoa = int(input("digite a idade do individuo: "))
+
+string_formatada = (f"o nome do individuo é {nome_da_pessoa},e á idade é {idade_da_pessoa} ")
+
+print(string_formatada)'''
+#segunda pergunta 
+#a varivel é igual uma caixa serve para guarda um dado para ser usado do decorrer do programa 
+#terceira pergunta
+ (X) String nome= “Pedro”
+(X) nome= “Pedro”
+(X) numero = 10
+(X) _numero= 10.5
+'''
+#quarta pergunta 
+Aritimetico 
+exemplos = (<  > , + - , / % ,) 
+Logico = or and not 
+  Textual = str,int,bool,float,for,while  '''
+#quinta pergunta
+numero1 = int(input("digite o primeiro numero: "))
+numero2 = int(input("digite o segundo numero: "))
+
+soma = numero1 + numero2
+
+print(soma)
+#sexta pergunta 
+numerovar = int(input("digite um numero: "))
+if numerovar > 10:
+    print(f"esse numero é maior que 10 ")
+else:
+    print(f"esse numero é menor que 10 ")'''
+
+
+    #setima pergunta
+numeropar = int(input("Digite um número: "))
+
+if numeropar % 2 == 0 and numeropar > 10:
+    print("O número  é par e maior que 10. ")
+else:
+    print("O número não satisfaz as condições. ")
