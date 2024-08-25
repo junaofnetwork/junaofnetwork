672d9a271a72daf16cdf412d09b790677b37de63
João eduardo <160231914+junaofnetwork@users.noreply.github.com>
2024-08-25 20:34:43 -0300

    Add files via upload

---
+++ Brincando.py
@@ -0,0 +1,11 @@
+def test_de_funcao(**kwargs):
+    print(kwargs)
+    print(kwargs['nome'],kwargs['sobrenome'],kwargs['idade'])
+
+
+
+    
+
+
+    
+test_de_funcao(nome="João", sobrenome="Eduardo", idade=23,  esperto=True)
