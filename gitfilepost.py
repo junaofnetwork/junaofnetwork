6425a2d205a78855772dd04fe5f8aa8a86f828e5
João eduardo <160231914+junaofnetwork@users.noreply.github.com>
2025-06-16 23:05:21 -0300

    fibonnacc
    Meu Primeiro Fibbonacci

---
+++ gitfilepost.py
@@ -0,0 +1,10 @@
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+    
+
+
+for i in range(10):
+    print(f"Fibonacci of {i} is {fibonacci(i)}")
